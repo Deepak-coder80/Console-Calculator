249f70b65c79399a08f494c13908098314f28b25
Deepak <souparnikad40@gmail.com>
2021-04-29 18:30:42 +0530

    output designed

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 ====================================================================
 Project Name : Sample Console Calculator
 Created By   : Deepak M S (GitHub:Deepak-coder80)
-Version      : 1.1.4
+Version      : 2.0
 Description  : This is a sample console calculator by c programming
 =====================================================================
 */
@@ -14,41 +14,43 @@ Description  : This is a sample console calculator by c programming
 
 int main()
 {
-   int choice;
+   int choice,n;
    float a,b;
-   int n;
+   
    printf("\t\t\t\t\t\t************************************************\n");
    printf("\t\t\t\t\t\t\tHai Welcome to the console calculator\n");
    printf("\t\t\t\t\t\t************************************************\n");
 
    do{
-   printf("Enter two numbers");
-   scanf("%f%f",&a,&b);
-   printf(" Enter 1 for Addition \n Enter 2 for Subtraction \n Enter 3 for Multiplication \n Enter 4 for Quotient of Division \n Enter Your Choice:");
-   scanf("%d",&choice);
+
+      printf("\nEnter two numbers");
+      scanf("%f%f",&a,&b);
+      printf("\n Enter 1 for Addition \n Enter 2 for Subtraction \n Enter 3 for Multiplication \n Enter 4 for Quotient of Division \n\n Enter Your Choice:");
+      scanf("%d",&choice);
 
 
-   switch(choice){
-   case 1:
-       printf("Sum = %.2f\n",(a+b));
-       break;
-    case 2:
-       printf("Difference = %.2f\n",(a-b));
-       break;
-    case 3:
-       printf("Product = %.2f\n",(a*b));
-       break;
-    case 4:
-       printf("Quotient of Division = %.3f\n",(a/b));
-       break;
-   
-    default:
-        printf("Invlaid Entry");
+      switch(choice){
+      case 1:
+         printf("\nSum = %.2f\n",(a+b));
+         break;
+      case 2:
+         printf("\nDifference = %.2f\n",(a-b));
+         break;
+      case 3:
+         printf("\nProduct = %.2f\n",(a*b));
+         break;
+      case 4:
+         printf("\nQuotient of Division = %.3f\n",(a/b));
+         break;
+      
+      default:
+         printf("\nInvlaid Entry\n");
 
-   };
+      };
 
-   printf("Do you want to continue?\n If yes, enter 1 else enter 0:");
-   scanf("%d",&n);
+      printf("\nDo you want to continue?\n If yes, enter 1 else enter 0:");
+      scanf("%d",&n);
+      
 
    }while(n==1);
     return 0;
